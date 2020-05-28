95398ca52a9b2c158c40b8a532b0f32811183433
Samuel Levy <samuel.neto@wooza.com.br>
2020-05-27 23:06:08 -0300

    create components with interdependents attributes

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -4,6 +4,7 @@ import { Course } from './course';
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html',
+  styleUrls: ['./course-list.component.scss'],
 })
 export class CourseListComponent implements OnInit {
   courses: Course[] = [];
@@ -13,17 +14,17 @@ export class CourseListComponent implements OnInit {
       {
         id: 1,
         name: 'Angular: Forms',
-        imageUrl: '',
+        imageUrl: '/assets/images/forms.png',
         price: 99.99,
         code: 'ABC-123',
         duration: 120,
-        rating: 5.4,
+        rating: 4.5,
         releaseData: 'December, 2, 2019',
       },
       {
         id: 2,
         name: 'Angular: HTTP',
-        imageUrl: '',
+        imageUrl: '/assets/images/http.png',
         price: 45.99,
         code: 'ABC-234',
         duration: 80,
